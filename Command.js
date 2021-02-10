43c923170c641c21ada6f602f5fea6f7db6b499f
Matthew McCall <matthewm3004@icloud.com>
2021-02-10 21:22:12 +0000

    Better command system

---
+++ Command.js
@@ -0,0 +1,27 @@
+class Command {
+
+    constructor(run) {
+        this.arguments = new Map();
+        this.run = run
+    }
+
+    call(msg, args, cmd = this) {
+        if (cmd.arguments.has(args[0])) {
+            this.call(msg, args, cmd.arguments.get(args[0]));
+        } else {
+            cmd.run(msg, args);
+        }
+    }
+
+    attach(cmd, cmdObj) {
+        this.arguments.set(cmd, cmdObj)
+        return this.arguments.get(cmd)
+    }
+
+    get(cmd) {
+        return this.arguments.get('cmd')
+    }
+
+}
+
+module.exports = Command;
