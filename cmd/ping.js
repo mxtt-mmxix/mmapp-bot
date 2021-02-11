8316ae4272ff64c9d525e0ea7cc4d74a88ab310a
Matthew McCall <matthewm3004@icloud.com>
2021-02-11 16:03:11 +0000

    Corrected calculations of reply time.

--- cmd/ping.js
+++ cmd/ping.js
@@ -5,7 +5,7 @@ const ping = new Command(async (msg) => {
 
     const sendTime = Date.now() - msg.createdAt;
     const rmsg = await msg.channel.send("Pinging...");
-    const replyTime = rmsg.createdAt - msg.createdAt
+    const replyTime = rmsg.createdTimestamp - (msg.createdTimestamp + sendTime);
     rmsg.edit(
         new Discord.MessageEmbed()
             .setColor('#0099ff')
@@ -13,7 +13,7 @@ const ping = new Command(async (msg) => {
             .addFields(
                 { name: "Send", value: sendTime + 'ms', inline: true },
                 { name: "Reply", value: replyTime + 'ms', inline: true },
-                { name: "Total", value: (replyTime + sendTime) + 'ms', inline: true },
+                { name: "Total", value: (rmsg.createdAt - msg.createdAt) + 'ms', inline: true },
             )
     );
     rmsg.edit('') // The "Pinging" stays there even after the MessageEmbed.
