6530bcfda1b70c71160a98f0770020dedbdffb25
Matthew McCall <matthewm3004@icloud.com>
2021-02-11 15:48:04 +0000

    Fancier ping

--- cmd/ping.js
+++ cmd/ping.js
@@ -1,8 +1,22 @@
+const Discord = require('discord.js')
 const Command = require('../Command')
 
 const ping = new Command(async (msg) => {
-    const rmsg = await msg.channel.send("pong!");
-    rmsg.edit("pong! `" + (rmsg.createdAt - msg.createdAt) + "ms`");
+
+    const sendTime = Date.now() - msg.createdAt;
+    const rmsg = await msg.channel.send("Pinging...");
+    const replyTime = rmsg.createdAt - msg.createdAt
+    rmsg.edit(
+        new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle("Pong!")
+            .addFields(
+                { name: "Send", value: sendTime + 'ms', inline: true },
+                { name: "Reply", value: replyTime + 'ms', inline: true },
+                { name: "Total", value: (replyTime + sendTime) + 'ms', inline: true },
+            )
+    );
+    rmsg.edit('') // The "Pinging" stays there even after the MessageEmbed.
 })
 
 module.exports = ping
