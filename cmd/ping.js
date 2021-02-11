91cab935f6568c2720247ffcf67dc837a594748d
Matthew McCall <matthewm3004@icloud.com>
2021-02-11 13:35:17 +0000

    Added profiling information to ping

--- cmd/ping.js
+++ cmd/ping.js
@@ -1,7 +1,8 @@
 const Command = require('../Command')
 
-const ping = new Command((msg) => {
-    msg.reply("pong!")
+const ping = new Command(async (msg) => {
+    const rmsg = await msg.channel.send("pong!");
+    rmsg.edit("pong! `" + (rmsg.createdAt - msg.createdAt) + "ms`");
 })
 
 module.exports = ping
