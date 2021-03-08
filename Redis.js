5576edb2824fc2d7e0be44a3568fbf4a856858da
Matthew McCall <matthewm3004@icloud.com>
2021-03-08 14:44:14 +0000

    Added more Redis stuff

--- Redis.js
+++ Redis.js
@@ -4,5 +4,9 @@ const { promisify } = require('util');
 
 module.exports = {
     get: promisify(client.get).bind(client),
-    set: promisify(client.set).bind(client)
+    set: promisify(client.set).bind(client),
+    hget: promisify(client.hget).bind(client),
+    llen: promisify(client.llen).bind(client),
+    lpush: promisify(client.lpush).bind(client),
+    ltrim: promisify(client.ltrim).bind(client)
 };
